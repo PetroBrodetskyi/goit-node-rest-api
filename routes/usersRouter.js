357daa63dc4b13a07e7619959fd3f296febfab67
PetroBrodetskyi <petro.brodetskyi@gmail.com>
2024-04-05 19:01:36 +0200

    VersionWithoutTests

--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
 import validateBody from "../helpers/validateBody.js";
 import { registerUserSchema, loginUserSchema } from "../schemas/usersSchemas.js";
-import { registerUser, loginUser, getCurrentUser, logoutUser} from "../controllers/usersControllers.js"
+import { registerUser, loginUser, getCurrentUser, logoutUser, uploadAvatar } from "../controllers/usersControllers.js"
 import { authanticate } from "../middlewares/authanticate.js";
 
 const usersRouter = express.Router();
@@ -12,6 +12,8 @@ usersRouter.post("/register", validateBody(registerUserSchema), registerUser);
 usersRouter.post("/login", validateBody(loginUserSchema), loginUser);
 
 usersRouter.get("/current", authanticate, getCurrentUser);
+
+usersRouter.patch("/avatars", authanticate, uploadAvatar);
 
 usersRouter.post("/logout", authanticate, logoutUser);
 
